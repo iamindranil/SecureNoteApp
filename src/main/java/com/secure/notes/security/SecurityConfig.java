c20df534c9c4c68fe89500fcc96741e2c6135762
iamindranil <indranilchakraborty692@gmail.com>
2026-01-31 13:59:21 +0530

    feat: project set up initialized

---
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.secure.notes.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults())
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+
+    //In Memory/DB Auth(InMemoryUserDetailsManager()/JdbcUserDetailsManager(DataSource datasource))
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource datasource){
+//        JdbcUserDetailsManager manager=new JdbcUserDetailsManager(datasource);
+//        if(!manager.userExists("user1")){
+//            manager.createUser(
+//                    User.withUsername("user1")
+//                            .password("{noop}password1")
+//                            .roles("USER")
+//                            .build()
+//            );
+//        }
+//        if(!manager.userExists("admin")){
+//            manager.createUser(
+//                    User.withUsername("admin")
+//                            .password("{noop}adminPass")
+//                            .roles("ADMIN")
+//                            .build()
+//            );
+//        }
+//        return manager;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/secure/notes/security/services/UserDetailsImpl.java
@@ -0,0 +1,97 @@
+package com.secure.notes.security.services;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.secure.notes.models.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID=1L;
+
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private boolean is2faEnabled;
+
+    private Collection<? extends GrantedAuthority> autorities;
+
+    public UserDetailsImpl(Long id,String username,String email,String password,boolean is2faEnabled,Collection<? extends GrantedAuthority> autorities){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.is2faEnabled=is2faEnabled;
+        this.autorities=autorities;
+    }
+
+    public static UserDetailsImpl build(User user){
+        GrantedAuthority authority=new SimpleGrantedAuthority(user.getRole().getRoleName().name());
+
+        return new UserDetailsImpl(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.isTwoFactorEnabled(),
+                List.of(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return autorities;
+    }
+
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this==o)return true;
+        if(o==null||this.getClass()!=o.getClass())return false;
+        UserDetailsImpl user=(UserDetailsImpl)o;
+        return Objects.equals(id,user.id);
+    }
+}
